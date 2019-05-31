1eed262b7d2dd10c234e16f8466e473f83f679b1
vitor <vitor.hoshino1@hotmail.com>
2019-05-31 16:32:59 -0400

    inserir e alterar

--- index.php
+++ index.php
@@ -2,11 +2,37 @@
 
 require_once("config.php" );
 require_once("Sql.php" );
+require_once("Usuario.php");
 
-$sql= new Sql();
+//Carrega um usuário
+///$root = new Usuario();
+//$root->loadbyId(1);
+//echo $root;
 
-$usuarios= $sql->select("SELECT * FROM tb_usuarios");
+///Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("asd");
+//echo json_encode($search);
 
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario ->login("LOCALHOST", "!@#$"); 
+//echo $usuario;
+
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(26);
+
+$usuario->update("professor", "!@#$%");
+echo $usuario;
 ?>
